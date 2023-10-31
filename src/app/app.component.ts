d5c15f89a817199aa96d5377435d03fb2005c94e
aespinozar19 <aespinozar19@gmail.com>
2023-10-30 20:24:25 -0500

    login, uso de Guards y token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth/services/auth.service';
+import { AuthStatus } from './auth/interfaces';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'authApp';
+  // title = 'authApp';
+
+  private authService = inject( AuthService );
+  private router      = inject( Router );
+
+  public finishedAuthCheck = computed<boolean>( () => {
+    console.log(this.authService.authStatus());
+    if( this.authService.authStatus() === AuthStatus.checking ) {
+      return false;
+    }
+    return true;
+  });
+
+  public authStatusChangeEffect = effect( () => {
+    // console.log( 'authStatus:', this.authService.authStatus() );
+    switch(this.authService.authStatus()) {
+      case AuthStatus.checking:
+        return;
+      case AuthStatus.authenticated:
+        this.router.navigateByUrl('/dashboard');
+        return;
+      case AuthStatus.notAuthenticated:
+        this.router.navigateByUrl('/auth/login');
+        return;
+
+    }
+
+  });
+
 }
