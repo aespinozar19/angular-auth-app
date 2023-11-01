971c28bd86987f45f041e440feac2af6a0867144
aespinozar19 <aespinozar19@gmail.com>
2023-10-31 19:25:25 -0500

    last commit

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
   // production: true
-  baseUrl: 'http://localhost:3000'
+  // baseUrl: 'http://localhost:3000'
+  baseUrl: 'https://nest-auth-backend-ajer.up.railway.app'
 }
